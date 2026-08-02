1624dfcf4f90cee98eec24f6c18594014af61824
kunal jadhav <kunaljadhav1982020@gmail.com>
2026-08-02 17:41:19 +0530

    Fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,47 @@
+// // import { defineConfig } from 'vite';
+// // import react from '@vitejs/plugin-react';
+// // import { fileURLToPath, URL } from 'node:url';
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   resolve: {
+// //     alias: {
+// //       '@': fileURLToPath(new URL('./src', import.meta.url)),
+// //     },
+// //   },
+// //   optimizeDeps: {
+// //     exclude: ['lucide-react'],
+// //   },
+// // });
+
+
+
+
+
+
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 // import { fileURLToPath, URL } from 'node:url';
 
-// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+
+//   // 👇 Add this line
+//   base: '/My_Dairy/',
+
 //   resolve: {
 //     alias: {
 //       '@': fileURLToPath(new URL('./src', import.meta.url)),
 //     },
 //   },
+
 //   optimizeDeps: {
 //     exclude: ['lucide-react'],
 //   },
 // });
+
+
 
 
 import { defineConfig } from 'vite';
@@ -22,16 +50,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-
-  // 👇 Add this line
-  base: '/My_Dairy/',
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
